9682200208a1f1352675dffa854fe1fe54fb6824
Vasilii Perfilev <vasily.perfilyev@gmail.com>
2021-12-08 21:05:51 -0800

    Add win logic

--- script.js
+++ script.js
@@ -1,48 +1,88 @@
 const Player = (name, symbol) => {
-    return {name, symbol};
+    let positionsList = [];
+    return {name, symbol, positionsList};
 };
 
 const player1 = Player("player1", "X");
 const player2 = Player("player2", "O");
 
-const displayController = ((player1, player2) => {
-    let activePlayer = player1;
-
-    const switchActivePlayer = () => {
-        activePlayer = (activePlayer === player1) ? player2 : player1;
-    }
-
-    const checkForWinner = (gameBoard, player, lastTurn) => {
-
-    }
-
-    const drawSymbol = (e) => {
-        if (e.target.textContent === ""){
-            e.target.textContent = activePlayer.symbol;
-            checkForWinner(this, activePlayer);
-            switchActivePlayer();
-        }
-    }
-    return {
-        drawSymbol,
-    }
-})(player1, player2);
-
-const gameBoard = ((gameBoardContainer, displayController) => {
-
-    gameBoardContainer.addEventListener("click", displayController.drawSymbol);
+const gameBoard = ((gameBoardContainer) => {
 
     const createNewBoard = () => {
+        gameBoardContainer.innerHTML = "";
         for (let i = 0; i < 9; i++){
             let cell = document.createElement("div");
+            cell['boardIndex'] = i;
             cell.classList.add("gridCell");
             gameBoardContainer.appendChild(cell);
         }
     }
 
-    return {
-        createNewBoard,
+    const resetGame = () => {
+        createNewBoard();
+        player1.positionsList = [];
+        player2.positionsList = [];
     }
-})(document.querySelector(".gameBoard"), displayController);
 
-gameBoard.createNewBoard();
+    return {
+        gameBoardContainer,
+        resetGame,
+        player1,
+        player2,
+    }
+})(document.querySelector(".gameBoard"));
+
+const displayController = ((gameBoard) => {
+    let activePlayer = gameBoard.player1;
+
+    const switchActivePlayer = () => {
+        activePlayer = (activePlayer === gameBoard.player1) ? gameBoard.player2 : gameBoard.player1;
+    }
+
+    const checkIfWin = (player, lastPosition) => {
+        let rowMatch = 0;
+        let columnMatch = 0;
+        let diagonalMatch = 0;
+
+        player.positionsList.forEach(position => {
+            if (Math.floor(position / 3) === Math.floor(lastPosition / 3)) rowMatch++;
+            if (position % 3 === lastPosition % 3) columnMatch++;
+            if ((position % 2 === lastPosition % 2) && !(Math.floor(position / 3) === Math.floor(lastPosition / 3))) diagonalMatch++;
+        })
+        console.log({rowMatch, columnMatch, diagonalMatch})
+        if (rowMatch === 3 || columnMatch === 3 || diagonalMatch === 3){
+            console.log(`${player.name} won`);
+            return true;
+        }
+        else if (gameBoard.player1.positionsList.length === 5)
+        {
+            console.log(`draw`);
+            return true;
+        }
+        {
+            return false;
+        }
+    }
+
+    const drawSymbol = (e) => {
+        if (e.target.textContent === ""){
+            e.target.textContent = activePlayer.symbol;
+            activePlayer.positionsList.push(e.target.boardIndex);
+            if (checkIfWin(activePlayer, e.target.boardIndex)){
+                setTimeout(gameBoard.resetGame, 5000)
+            }
+            else 
+            {
+                switchActivePlayer();
+            }
+        }
+    }
+
+    gameBoard.gameBoardContainer.addEventListener("click", drawSymbol);
+
+    return {
+        drawSymbol,
+    }
+})(gameBoard);
+
+gameBoard.resetGame();
