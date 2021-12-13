7def8712df8a5f6b29d17764dc2c6f391595d414
Vasilii Perfilev <vasily.perfilyev@gmail.com>
2021-12-13 10:08:19 -0800

    Add AI minimax algorithm

--- script.js
+++ script.js
@@ -38,30 +38,109 @@ const gameController = ((restartBtn) => {
         return activePlayer
     }
 
-    const checkIfWin = (player, lastPosition) => {
+    const checkIfWin = (positionsList) => {
         let rowMatch = 0;
         let columnMatch = 0;
         let diagonalMatch = 0;
+        let lastPosition = positionsList[positionsList.length - 1]
+        let positionsWithoutLast = positionsList.slice(0, -1)
 
-        player.positionsList.forEach(position => {
-            if (Math.floor(position / 3) === Math.floor(lastPosition / 3)) {
-                rowMatch++;
-            } else if (position % 3 === lastPosition % 3) {
-                columnMatch++;
-            } else if ((position % 2 === 0) && (lastPosition % 2 === 0)) {
-                diagonalMatch++;
+        //positionsWithoutLast.forEach(position => {
+        for (let i = 0; i < positionsList.length; i++){
+            for (let j = i + 1; j < positionsList.length; j++){
+                let position = positionsList[i];
+                let lastPosition = positionsList[j];
+                if (Math.floor(position / 3) === Math.floor(lastPosition / 3)) {
+                    rowMatch++;
+                } else if (position % 3 === lastPosition % 3) {
+                    columnMatch++;
+                } else if ((position % 2 === 0) && (lastPosition % 2 === 0)) {
+                    diagonalMatch++;
+                }
             }
-        })
+            // if (Math.floor(position / 3) === Math.floor(lastPosition / 3)) {
+            //     rowMatch++;
+            // } else if (position % 3 === lastPosition % 3) {
+            //     columnMatch++;
+            // } else if ((position % 2 === 0) && (lastPosition % 2 === 0)) {
+            //     diagonalMatch++;
+            // }
+        }//)
+        //console.log({rowMatch, columnMatch, diagonalMatch});
 
-        if (rowMatch === 2 || columnMatch === 2 || diagonalMatch === 2) {
-            console.log(`${player.name} won`);
-            return true;
-        } else if (player.positionsList.length === 4) {
-            console.log(`draw`);
-            return true;
+        if (rowMatch === 3 || columnMatch === 3 || diagonalMatch >= 3) {
+            //won
+            return "win";
+        } else if (positionsList.length === 5) {
+            //draw
+            return "draw";
         } {
             return false;
         }
+    }
+
+    const currentState = () => {
+        const freePositionsList = [];
+        Array.from(gameBoard.gameBoardContainer.children).forEach((child) => {
+            if (child.textContent == "") {
+                freePositionsList.push(child.boardIndex);
+            }
+        })
+        return {
+            p1Positions: [...player1.positionsList],
+            p2Positions: [...player2.positionsList],
+            freePositionsList
+        }
+    }
+
+    const createNewState = (currentState, position, isP1) => {
+        let positionIndex = currentState.freePositionsList.indexOf(position);
+        let newState = JSON.parse(JSON.stringify(currentState));
+        if (isP1) {
+            newState.p1Positions.push(position);
+            newState.freePositionsList.splice(positionIndex, 1);
+        } else {
+            newState.p2Positions.push(position);
+            newState.freePositionsList.splice(positionIndex, 1);
+        }
+        return newState;
+    }
+
+    const scoreMove = (currentState, depth, isMaximazingPlayer) => {
+        if (depth === 0 || checkIfWin(currentState.p1Positions) || checkIfWin(currentState.p2Positions)) {
+            if (checkIfWin(currentState.p1Positions) === "win") return -100;
+            if (checkIfWin(currentState.p1Positions) === "draw") return 0;
+            if (checkIfWin(currentState.p2Positions) === "win") return 100 - depth;
+        }
+        if (isMaximazingPlayer) {
+            let value = -100;
+            currentState.freePositionsList.forEach(position => {
+                let newState = createNewState(currentState, position, false)
+                value = Math.max(value, scoreMove(newState, depth - 1, false));
+                //console.log(newState.p1Positions, newState.p2Positions, value);
+            })
+            return value;
+        } else {
+            let value = 100;
+            currentState.freePositionsList.forEach(position => {
+                let newState = createNewState(currentState, position, true)
+                value = Math.min(value, scoreMove(newState, depth - 1, true));
+                //console.log(newState.p1Positions, newState.p2Positions, value);
+            })
+            return value;
+        }
+    }
+
+    const chooseMove = (currentState) => {
+        moveScores = {};
+
+        currentState.freePositionsList.forEach(position => {
+            let newState = createNewState(currentState, position, false);
+            let value = scoreMove(newState, 8, false);
+            moveScores[position] = value;
+        })
+
+        console.log(moveScores);
     }
 
     const resetGame = () => {
@@ -80,37 +159,48 @@ const gameController = ((restartBtn) => {
         getActivePlayer,
         checkIfWin,
         resetGame,
-        switchActivePlayer
+        switchActivePlayer,
+        currentState,
+        chooseMove,
     }
 })(document.querySelector(".restartBtn"));
 
 const displayController = ((infoLabel) => {
 
     let active = true;
+    let result = false;
 
     const drawSymbol = (e) => {
         if (active) {
             activePlayer = gameController.getActivePlayer()
             if (e.target.textContent === "") {
                 e.target.textContent = activePlayer.symbol;
-                if (gameController.checkIfWin(activePlayer, e.target.boardIndex)) {
+                activePlayer.positionsList.push(e.target.boardIndex);
+                result = gameController.checkIfWin(activePlayer.positionsList);
+                if (result) {
                     active = false;
                 } else {
-                    activePlayer.positionsList.push(e.target.boardIndex);
                     gameController.switchActivePlayer();
                 }
                 changeInfoLabel(gameController.getActivePlayer());
+                
+                if (activePlayer.name == "Player 1"){
+                    let currentState = gameController.currentState()
+                    gameController.chooseMove(currentState);
+                }
             }
         }
     }
 
     const changeInfoLabel = (activePlayer) => {
-        if (active === true) infoLabel.textContent = `${activePlayer.name} turn`;
-        if (active === false) infoLabel.textContent = `${activePlayer.name} won!`;
+        if (result === false) infoLabel.textContent = `${activePlayer.name} turn`;
+        if (result === "win") infoLabel.textContent = `${activePlayer.name} won!`;
+        if (result === "draw") infoLabel.textContent = `Draw!`;
     }
 
     const setActive = (bool) => {
         active = bool;
+        result = !bool;
     }
 
     gameBoard.gameBoardContainer.addEventListener("click", drawSymbol);
